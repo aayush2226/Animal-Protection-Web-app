9ad3c1bd318134659e93aa6a8528a993e335259e
Aayush Bhati <Aayush.2226it1119@kiet.edu>
2025-07-30 16:02:39 +0530

    Add files via upload

---
+++ src/constants/FooterItems.js
@@ -0,0 +1,28 @@
+import * as Icons from "react-icons/fa";
+
+export const footerItems = [
+  {
+    id: 1,
+    title: "Ana Sayfa",
+    path: "/",
+    icon: <Icons.FaHome />,
+  },
+  {
+    id: 2,
+    title: "Barınak İlanları",
+    path: "/barinaklar",
+    icon: <Icons.FaNewspaper />,
+  },
+  {
+    id: 3,
+    title: "Veterinere Danış",
+    path: "/veterinere-danis",
+    icon: <Icons.FaUserNurse />,
+  },
+  {
+    id: 4,
+    title: "Sosyal Pati",
+    path: "/sosyal-pati",
+    icon: <Icons.FaPaw />,
+  }
+];
